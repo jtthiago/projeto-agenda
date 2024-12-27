824c3dc7f298a5fc2f9da5b9de969727b46aa5ab
Thiago Haubrick <jtthiago42@outlook.com>
2024-12-27 10:09:41 -0300

    Validando campos do formulário com clean, clean_field e ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -3,7 +3,6 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -11,7 +10,7 @@ class ContactForm(forms.ModelForm):
                 'placeholder': 'Seu nome aqui',
             }
         ),
-        label = 'Primeiro Nome',
+        label='Primeiro Nome',
         help_text='Texto de ajuda',
     )
 
@@ -24,15 +23,20 @@ class ContactForm(forms.ModelForm):
             'first_name', 'last_name', 'phone', 'email',
         )
 
-def clean(self):
-    #cleaned_data = self.cleaned_data
+    def clean(self):
+        # Obtendo os dados limpos
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-    self.add_error(
-        'first_name',
-        ValidationError(
-            'Mensagem de erro',
-            code='invalid'
-        )
-    )
+        # Verificando se `first_name` é igual a `last_name`
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
-    return super().clean()
+        # Retornando os dados limpos
+        return super().clean()
