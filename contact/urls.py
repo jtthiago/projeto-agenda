d40a420fd92587414b2231e2142abbd223b5127f
Thiago Haubrick <jtthiago42@outlook.com>
2024-12-31 11:43:04 -0300

    Criando a view, url e template para 'delete' (apagar contactos)

--- contact/urls.py
+++ contact/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 
     path('contact/<int:contact_id>/', views.contact, name='contact' ),
     path('contact/<int:contact_id>/update/', views.update, name='update' ),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete' ),
     path('contact/create/', views.create, name='create' ),
     
 ]
